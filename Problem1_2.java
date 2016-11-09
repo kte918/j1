dab6a8e648e6f877cb8f2300af0dadb48e267a10
kte918 <kte918@naver.com>
2016-11-10 08:56:05 +0900

    Add files via upload

---
+++ Problem1_2.java
@@ -0,0 +1,38 @@
+class Problem1_2 
+{
+	public static void main(String[] args) 
+	{
+	int i = 0;
+	int count = 0;
+	while(i<101)
+	{
+		if(i%4==0 && i%5!=0)
+		{
+			count++;
+		}
+		i++;
+	}
+
+	int [] list = new int[count];
+
+	int j = 0;
+	int count2 = 0;
+	while(j<101)
+	{
+		if(j%4==0 && j%5!=0)
+		{
+		list[count2] = j;
+		count2++;
+		}
+		j++;
+	}
+
+	int sum = 0;
+	for(i=0;i<count;i++)
+	{
+	System.out.println(list[i]);
+	sum = sum + list[i];
+	}
+	System.out.println("Total sum is "+sum);
+	}
+}
