3f0cb238cd2b2b2d52e3ac1e35584c4f1ec7294a
kte918 <kte918@naver.com>
2016-11-14 18:45:33 +0900

    Add files via upload
    교수님꼐서 이름 바꾸라고 하셔서 다시올립니다. 원본은 Problem1, Problem1_2 입니다

---
+++ Numbers.java
@@ -0,0 +1,36 @@
+class Numbers
+{
+	public static void main(String[] args) 
+	{
+	int i = 0;
+	int count = 0;
+	while(i<101)
+	{
+		if(i%4==0 && i%5!=0)
+		{
+			count++;
+		}
+		i++;
+	}
+
+	int [] list = new int[count];
+
+	int j = 0;
+	int count2 = 0;
+	while(j<101)
+	{
+		if(j%4==0 && j%5!=0)
+		{
+		list[count2] = j;
+		count2++;
+		}
+		j++;
+	}
+
+	for(i=0;i<count;i++)
+	{
+	System.out.println(list[i]);
+	}
+
+	}
+}
